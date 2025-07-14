566ca22c62b1c3741e9dd9860492ff30a2218666
Vilax <luminousvilax@gmail.com>
2025-07-14 16:57:47 +0800

    now we have a little works

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+struct Work {
+    title: String,
+    content: String
+}
+
+impl Work {
+    pub fn new(title: String, content: String) -> Self{
+        Self {title, content}
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let arg_len = args.len();
+    if arg_len < 2{
+        println!("Usage: [title] content");
+        return;
+    }
+    // init
+    let mut works: Vec<Work> = vec![];
+    if arg_len >= 3{
+        works.push(Work::new(args[1].clone(), args[2].clone()));
+    } else if args[1].clone() == "create" {
+        interactive();
+        return;
+    } 
+    else{
+        works.push(Work::new(String::new(), args[1].clone()));
+    }
+    println!("Todo list:");
+    for work in &works {
+        println!("------------------");
+        println!("Title: {}\nContent: {}", work.title, work.content);
+    }
+}
+
+fn interactive() {
+    
+}
