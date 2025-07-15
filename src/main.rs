da03b8f0bed7f373e9857b7166999573877f7ce6
Vilax <luminousvilax@gmail.com>
2025-07-15 18:52:20 +0800

    use clap to parse ctl command

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
 mod todo;
-use crate::todo::core::Work;
-use crate::todo::create::interactive;
-use crate::todo::list::todo_list;
+use crate::todo::core::{TodoCommand, Work};
 use crate::todo::storage::{read_works, save_works};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = "Todo cli")]
+struct Program {
+    #[command(subcommand)]
+    pub command: TodoCommand,
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let arg_len = args.len();
-    if arg_len < 2 {
-        println!("Usage: [title] content");
-        return;
-    }
+    let args = Program::parse();
     // init
     let file_path = "data/todo.json";
     let mut works: Vec<Work> = read_works(file_path);
-    match args[1].clone().as_str() {
-        "create" => {
-            let mut new_works = interactive();
-            works.append(&mut new_works);
-        }
-        "list" => {
-            todo_list(&works);
-        }
-        _ => {
-            if arg_len >= 3 {
-                works.push(Work::new(args[1].clone(), args[2].clone()));
+    match args.command {
+        TodoCommand::Create {
+            interactive_mode,
+            title,
+            content,
+        } => {
+            if interactive_mode {
+                let mut new_works = todo::create::interactive();
+                works.append(&mut new_works);
             } else {
-                works.push(Work::new(String::new(), args[1].clone()));
+                works.push(Work::new(title.unwrap_or_default(), content));
             }
+        }
+        TodoCommand::List => {
+            todo::list::todo_list(&works);
+        }
+        TodoCommand::Clear => {
+            works.clear();
         }
     }
     save_works(file_path, &works);
--- src/todo/core.rs
+++ src/todo/core.rs
@@ -1,18 +1,39 @@
+use clap::Subcommand;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct Work {
     pub title: String,
-    pub content: String
+    pub content: String,
 }
 
 impl Work {
-    pub fn new(title: String, content: String) -> Self{
-        Self {title, content}
+    pub fn new(title: String, content: String) -> Self {
+        Self { title, content }
     }
 
-    pub fn show(&self){
+    pub fn show(&self) {
         println!("------------------");
         println!("Title: {}\nContent: {}", self.title, self.content);
     }
+}
+
+#[derive(Debug, Clone, Subcommand)]
+pub enum TodoCommand {
+    /// Add todo
+    Create {
+        /// create in interactive mode
+        #[arg(short)]
+        interactive_mode: bool,
+        /// The title of the todo (optional)
+        #[arg(short, long)]
+        title: Option<String>,
+        /// The content of the todo
+        #[arg(short, long)]
+        content: String,
+    },
+    /// List all the todo works
+    List,
+    /// Clear all works
+    Clear
 }
